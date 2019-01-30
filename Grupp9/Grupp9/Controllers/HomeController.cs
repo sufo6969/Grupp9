1045ec8bc004fd4cab7275aad5c5bebb9c34f20d
Silfverlin <43538895+Silfverlin@users.noreply.github.com>
2019-01-30 13:23:02 +0100

    Authorize så man kommer till registrering när man inte är inloggad

--- Grupp9/Grupp9/Controllers/HomeController.cs
+++ Grupp9/Grupp9/Controllers/HomeController.cs
@@ -1,18 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Grupp9.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ActionResult Index()
         {
             return View();
         }
 
+        [Authorize]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +18,7 @@ namespace Grupp9.Controllers
             return View();
         }
 
+        [Authorize]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
