e2ec5af9f16dfc09f2ea74657cc510dbea232f13
sufo6969 <43465892+sufo6969@users.noreply.github.com>
2019-01-30 14:24:04 +0100

    test

---
+++ Grupp9/Grupp9/Controllers/FormellaInläggController.cs
@@ -0,0 +1,47 @@
+﻿using Datalager;
+using Grupp9.Models;
+using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Grupp9.Controllers
+{
+    public class FormellaInläggController : Controller
+    {
+        // GET: FormellaInlägg
+        public ActionResult Index()
+        {
+            
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Skriv(FormellaInläggViewModell model)
+        {
+            var db = new InfoDbContext();
+            var currentUser = "testtext";
+
+            db.FormellaInläggen.Add(new Datalager.Models.FormellaInlägg
+            {
+                UserId = currentUser,
+                Titel = model.titel,
+                Text = model.text
+
+
+
+
+            });
+
+            db.SaveChanges();
+
+            return View(model);
+
+
+        }
+    }
+}
---
+++ Grupp9/Grupp9/Models/FormellaInläggViewModell.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Grupp9.Models
+{
+    public class FormellaInläggViewModell
+    {
+        [StringLength(300, ErrorMessage = "Texten kan inte vara längre än 300 tecken") ]
+        public string text { get; set; }
+        [StringLength(50, ErrorMessage = "Texten kan inte vara längre än 50 tecken")]
+        public string titel { get; set; }
+    }
+}
